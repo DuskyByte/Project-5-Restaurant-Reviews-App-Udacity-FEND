e08085335b7cff3178bb63f214ab8b987989d2a0
DuskyByte <samuel.j.smith.thrasher@gmail.com>
2018-09-20 08:50:42 -0700

    fix: added mapbox api key to allow maps to render on all pages

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1IjoiZHVza3lieXRlIiwiYSI6Ildhd2ZQcDQifQ.Eo0o3WqdZXRkZ_8nJBxrFw',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
